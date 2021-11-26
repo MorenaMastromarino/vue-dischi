aedd81e74dc8e931d453421582030356cf4c51de
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-26 16:32:54 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  
+  <div>
+    <Header />
+    <Main />
+  </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
   }
 }
 </script>
 
 <style lang="scss">
-  @import '~bootstrap/scss/bootstrap.scss';
-  @import '~@fontsource/montserrat/index.css';
+  @import './assets/style/vars.scss';
+  @import './assets/style/generals.scss';
 
   
   
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="d-flex align-items-center">
+    <img src="../assets/img/spotify-logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/style/vars.scss';
+
+  header {
+    height: 70px;
+    padding: 0 20px;
+    background-color: $primary-color;
+    img {
+      width: 45px;
+    }
+  }
+</style>
