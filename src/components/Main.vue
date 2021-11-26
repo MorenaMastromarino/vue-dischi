a5c4ea5af63c89eec570d03306187691d08d63c9
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-26 17:44:52 +0100

    stampo una card statica

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,34 @@
 <template>
   <main>
-    MAIN
+   
+    <div class="container py-5">
+
+      <SongsList />      
+
+    </div>
+
   </main>
 </template>
 
 <script>
+import SongsList from './SongsList.vue';
+
 export default {
   name: 'Main',
 
+  components: {
+    SongsList,
+
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import '../assets/style/vars.scss';
 
+  main {
+    min-height: calc(100vh - 70px);
+    background-color: darken($primary-color, 6%);    
+    
+  }
 </style>
---
+++ src/components/SongsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row row-cols-5 gx-4 gy-3" >
+
+    <div class="col">
+      <div class="card p-3 text-center">
+        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+        <h3 class="py-3">New jersey</h3>
+        <p class="mb-0">Bon Jovi</p>
+        <p class="">1988</p>
+      </div>
+    </div>     
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SongsList',
+
+}
+</script>
+
+<style lang="scss" scoped>
+ @import '../assets/style/vars.scss';
+
+  .card {
+    background-color: $primary-color;
+    
+    
+    h3 {
+      text-transform: uppercase;
+      font-size: 20px;      
+    }
+    p {
+      font-size: 14px;
+      color: #6B8080;
+    }
+  }
+</style>
